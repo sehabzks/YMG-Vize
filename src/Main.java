7a43866b6f119675ad08fbaea719340f6ef67b07
sehabzks <99829809+sehabzks@users.noreply.github.com>
2024-07-03 00:28:15 +0300

    deneme

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("HelloadasdasdadASDAsdfsf selam ben seha world!");
+        System.out.println(" selam ben seha ");
     }
 
 }
